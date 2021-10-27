d51252e55183879ce65e1f013c4c7f4cc7cc73c8
Fapse <fb-online@posteo.de>
2021-10-27 19:00:09 +0200

    Initial Commit

---
+++ Pappsatt/PappsattApp.swift
@@ -0,0 +1,17 @@
+//
+//  PappsattApp.swift
+//  Pappsatt
+//
+//  Created by Fabian Braig on 27.10.21.
+//
+
+import SwiftUI
+
+@main
+struct PappsattApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
